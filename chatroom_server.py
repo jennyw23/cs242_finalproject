0632d9a3f0509a6205eb622b2a8515864c0a29d7
jennyw23 <57296933+jennyw23@users.noreply.github.com>
2020-10-14 13:17:57 -0400

    Add files via upload
    new updated versions

--- chatroom_server.py
+++ chatroom_server.py
@@ -1,13 +1,78 @@
-import threading
+from threading import Thread
 import socket
 
 HOST = 'localhost'
 PORT = 1060
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(HOST, PORT)
-socket.listen()
-print('Waiting for client connections')
+class Server(Thread):
+    def __init__(self):
+        super().__init__()
+        self.host = '127.0.0.1'
+        self.port = 1060
+        self.clients = []
+    
+    def run(self):
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create threads for each client that connects
-while True:
+        # this part is new info; i think we should ask Christine's advice on whether to use
+        # but it essentially allows us to use an old port without waiting
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        serverSocket.bind((HOST, PORT))
+
+        # this listening socket only establishes TCP connections. We handle the connecting to threads in the clientThread class
+        serverSocket.listen(1)
+        print('Waiting for client connections at', HOST)
+
+        # Create threads for each client that connects
+        while True:
+            connection, address = serverSocket.accept()
+            print('Client connected') #add which client connected to
+
+            # create thread for client
+            new_client_thread = ClientThread(connection, address, self)
+
+            # begin the new thread
+            new_client_thread.start()
+
+            # add thread to active connections 
+            self.clients.append(new_client_thread)
+            print('Ready to receive messages from', connection.getpeername())
+
+
+    def broadcast(self, message, source):
+        for client in self.clients:
+            if client.address != source:
+                client.send(message)
+    
+    def remove_client(self, client):
+        self.clients.remove(client)
+
+class ClientThread(Thread):
+    def __init__(self, connection, address, server):
+        super().__init__()
+        self.connection = connection
+        self.address = address
+        self.server = server
+    
+    def run(self):
+        while True:
+            
+            message = self.connection.recv(1024).decode('ascii')
+
+            if message != "":
+                print(self.address, ': ', message)
+                self.server.broadcast(message, self.address)
+            else:
+                # user has "quit" the chat room, so we need to exit the thread
+                print('{} has left the chat room'.format(self.address))
+                self.connection.close()
+                server.remove_client(self)
+                return       
+    
+    def send(self, message):
+        self.connection.sendall(message.encode('ascii'))
+
+if __name__ == '__main__':
+    server = Server()
+    server.start()
---
+++ client.py
@@ -0,0 +1,142 @@
+import threading
+import socket
+import argparse
+import os
+
+terriIP = 'localhost'
+terriHOST = '192.168.8.104'
+
+class Send(threading.Thread):
+    """
+    This class listens for user input from the command line.
+    """
+    def __init__(self, sock, name):
+        """
+        Initialize instance variables.
+        sock: The socket object to be connected
+        name: Inputted username
+        """
+        super().__init__()
+        self.sock = sock
+        self.name = name
+
+    def run(self):
+        """
+        Takes user input (from command line) and sends to the server
+        Typing 'QUIT' closes connection between client and server
+        """
+        while True:
+            message = input('{}: '.format(self.name))
+
+            # Type 'QUIT' to leave the chatroom
+            if message.upper() == 'QUIT':
+                self.sock.sendall('Server: {} has just left the chat.'.format(self.name).encode('ascii'))
+                break
+            
+            # Send message to server for broadcasting
+            else:
+                self.sock.sendall('{}: {}'.format(self.name, message).encode('ascii'))
+        
+        print('\nQuitting...')
+        self.sock.close()
+        os._exit(0)
+
+
+class Receive(threading.Thread):
+    """
+    This class listens for incoming messages from the server.
+    """
+    def __init__(self, sock, name):
+        """
+        Initialize instance variables.
+        sock: The socket object to be connected
+        name: Inputted username
+        """
+        super().__init__()
+        self.sock = sock
+        self.name = name
+
+    def run(self):
+        """
+        Receives data from the server and prints in terminal. 
+        Note that stops listening for incoming data when either end has closed socket.
+        """
+        while True:
+            message = self.sock.recv(1024).decode('ascii')
+
+            if message:
+                print('\r{}\n{}: '.format(message, self.name), end = '')
+            
+            else:
+                # Server has closed the socket, exit the program
+                print('\nOh no, we have lost connection to the server!')
+                print('\nQuitting...')
+                self.sock.close()
+                os._exit(0)
+
+class Client:
+    """
+    This class creates a client object
+    """
+    def __init__(self, host, port):
+        """
+        host: The IP address of the server's listening socket.
+        port: The port number of the server's listening socket.
+        sock: The connected socket object.
+        name: The username of the client.
+        """
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = None
+    
+    def start(self):
+        """
+        Creates client-server connection. 
+        Records username from user input.
+        Creates and starts the Send and Receive threads.
+        Notifies other connected clients client has just entered chatroom.
+        Returns: A Receive object representing the receiving thread.
+        """
+        print('Trying to connect to {}:{}...'.format(self.host, self.port))
+        self.sock.connect((self.host, self.port))
+        print('Successfully connected to {}:{}'.format(self.host, self.port))
+        
+        print()
+        self.name = input('Please input your screen name: ')
+
+        print()
+        print('Welcome, {}!'.format(self.name))
+
+        # Create send and receive threads
+        send = Send(self.sock, self.name)
+        receive = Receive(self.sock, self.name)
+
+        # Start send and receive threads
+        send.start()
+        receive.start()
+
+        self.sock.sendall('Server: {} has joined the chat. Say hi!'.format(self.name).encode('ascii'))
+        print("\rYou're now ready to send and receive messages! To leave the chatroom, type 'QUIT'\n")
+        print('{}: '.format(self.name), end = '')
+
+        return receive
+
+
+def main(host, port):
+    """
+    Initializes and runs the GUI application.
+    host: The IP address of the server's listening socket.
+    port: The port number of the server's listening socket.
+    """
+    client = Client(host, port)
+    receive = client.start()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatroom Server')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+    main(args.host, args.p)
